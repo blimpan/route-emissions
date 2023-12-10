1e6b11c03d092160777fef6e6d4f5a0ee0c3b825
linus <linusbalt3r@gmail.com>
2023-12-10 20:00:52 +0100

    Removed some template stuff and added first own route.

---
+++ src/app/aboutme/page.jsx
@@ -0,0 +1,9 @@
+
+export default function AboutMe() {
+    return (
+        <main>
+            <h1>About Me</h1>
+            <p>My name is Linus!</p>
+        </main>
+    );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Home Page</h1>
+      <p>Hope you like my website!</p>
+    </main>
+  )
+}
