aa8c868c1b00a53ab03132537334f3fd67209573
linus <linusbalt3r@gmail.com>
2023-12-15 22:56:27 +0100

    Map renders only after response from Routes API and is zoomed properly.

--- src/app/route/customMap.tsx
+++ src/app/route/customMap.tsx
@@ -1,34 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { GoogleMap, Polyline, LoadScript } from '@react-google-maps/api';
 import PolylineDecoder from 'polyline';
 import { API_KEY } from '../../../lib/mapsAPIConfig';
 
-export default function MapComponent(props:any) {
-    
-  const decodedPath = PolylineDecoder.decode(props.encodedPolyline);
-  const pathAsLatLng = decodedPath.map(coord => ({ lat: coord[0], lng: coord[1] }));
+// Define libraries outside of the component
+const myLibraries = ['geometry'];
 
+export default function MapComponent(props: any) {
+  const decodedPath = PolylineDecoder.decode(props.encodedPolyline);
+  const pathAsLatLng = decodedPath.map((coord) => ({ lat: coord[0], lng: coord[1] }));
+
+  const [map, setMap] = useState<any>(null);
+
+  const onLoad = (map: any) => {
+    setMap(map);
+  };
+
+  const onUnmount = () => {
+    setMap(null);
+  };
+
+  useEffect(() => {
+    if (map) {
+      const bounds = new window.google.maps.LatLngBounds();
+      decodedPath.forEach((coord: number[]) => {
+        bounds.extend(new window.google.maps.LatLng(coord[0], coord[1]));
+      });
+
+      map.fitBounds(bounds);
+    }
+  }, [map, props.encodedPolyline]);
 
   const mapContainerStyle = {
     width: '100%',
     height: '400px',
   };
-  
-  const position = {
+
+  const startPosition = {
     lat: decodedPath[0][0],
     lng: decodedPath[0][1],
   };
-  
 
   return (
-    <LoadScript
-      googleMapsApiKey = {API_KEY}
-      libraries={['geometry']}
-    >
+    <LoadScript 
+      googleMapsApiKey={API_KEY}
+      libraries={myLibraries as any}> {/* moved expression outside of function to get rid of warning in browser */}
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
-        center= {position}
-        zoom={13}
+        zoom={20}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
       >
         <Polyline
           path={pathAsLatLng}
@@ -41,4 +62,4 @@ export default function MapComponent(props:any) {
       </GoogleMap>
     </LoadScript>
   );
-};
+}
--- src/app/route/page.tsx
+++ src/app/route/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 
+import React, { useState } from 'react';
 import Link from "next/link"
 import getRoute from "../../../lib/mapsSource";
 import MapComponent from "./customMap";
 
+/*
 const requestData = {
   origin: {
     "address": "KTH Royal Institute of Technology, Brinellvägen 8, 114 28 Stockholm",
@@ -12,26 +14,51 @@ const requestData = {
     "address": "Karolinska Institute, Solnavägen 1, 171 77 Solna",
   },
   travelMode: 'WALK',
-  /*
-  routingPreference: "TRAFFIC_AWARE",
-  routeModifiers: {
-    "avoidHighways": false
+  
+  // routingPreference: "TRAFFIC_AWARE",
+  // routeModifiers: {
+  //   "avoidHighways": false
+  // },
+  
+
+  languageCode: 'en-US',
+  units: 'METRIC',
+};
+*/
+
+const requestData = {
+  origin: {
+    "address": "T-Centralen, 111 20 Stockholm",
   },
-  */
+  destination: {
+    "address": "Bangatan 1, 222 21 Lund",
+  },
+  travelMode: 'DRIVE',
+  
+  // routingPreference: "TRAFFIC_AWARE",
+  // routeModifiers: {
+  //   "avoidHighways": false
+  // },
+  
 
   languageCode: 'en-US',
   units: 'METRIC',
 };
 
-
 export default function RoutePage() {
 
-  let polylineStr: string;
+  const [buttonPressed, setButtonPressed] = useState(false);
+  const [apiResponse, setApiResponse] = useState(null);
   
   async function onDoApiThing() {
     const responseData = getRoute(requestData);
     const response = await responseData;
+
+    setApiResponse(response);
+    setButtonPressed(true);
+
     console.log(response);
+    // console.log(response.routes[0].polyline.encodedPolyline)
   }
 
   return (
@@ -43,11 +70,13 @@ export default function RoutePage() {
       onClick={onDoApiThing}
       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Click to do API thing
       </button>
-      <div className="debug flex w-full justify-center">
-        <div className="debug w-1/2">
-          <MapComponent encodedPolyline="uwfiJqihmBK@MbA^ES^YRJn@ADEU@?DNG\?T{@rALh@aB~BZlAVzAb@xANx@PVFZIPQP?^PvCtCfEVVVIHGFd@EHCRB@GXDPEPXx@Q`CeC~Ho@jBKJ{ApEeAdCWp@Rb@KZkBrDg@jAcApCT|@\jAABJf@@ABLr@i@ZGHFHVVbCNlBcA`Dw@zBAR@TID]dBKXBNGNMvBCfC@^Uv@SzAChB?rBEp@Ub@_@f@zA`G~@vDA^yApKIRQnAWn@QNW@EES`@G`@c@lAEj@AhA_@OeAKQLqArBWh@E\_CvFe@fAKNLl@VXJDD^b@nBn@nF?jA|ArPLx@Jh@JfBB|@W|d@@`@AzAE`@Qr[@^CtFC`AAvBBd@It@e@pA]j@IHy@zAYJSj@LTSj@Qr@MTsAjHg@dF]vBe@zAoDbIqAbDCCETHFGVWPXVDOPJMXNZMVkBlFyAtEM`@Qz@]`A@rAAXFnADvBDl@BlCg@F?NWD"/>
+      {buttonPressed && apiResponse && (
+        <div className="debug flex w-full justify-center">
+          <div className="debug w-1/2">
+            <MapComponent encodedPolyline={apiResponse.routes[0].polyline.encodedPolyline} />
+          </div>
         </div>
-      </div>
+      )}
     </>
   )
 }
